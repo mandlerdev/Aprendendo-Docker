e7335531e1b2c4faab5203d1f27ca714a5d83920
Rafael Mandler <mandlerrafael123@gmail.com>
2026-08-11 18:29:19 -0300

    criando conexoes com o db

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,22 @@
-export const createUser = (req, res) => {
+import User from "../models/User.js";
+import crypto from "node:crypto";
 
-    res.status(200).json({ msg: 'Funcionou'})
-} 
+export const createUser = async (req, res) => {
+  const userToCreate = {
+    id: crypto.randomUUID(),
+    name: req.body.name,
+    age: req.body.age,
+    email: req.body.email,
+  };
+
+  const user = await User.create(userToCreate);
+  res.status(201).json(user);
+};
 
 export const getAllUsers = (req, res) => {
-
-    res.status(200).json({ msg: 'Funcionou'})
-} 
+  res.status(200).json({ msg: "Funcionou" });
+};
 
 export const deleteUser = (req, res) => {
-
-    res.status(200).json({ msg: 'Funcionou'})
-} 
+  res.status(200).json({ msg: "Funcionou" });
+};
--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
-import express from 'express'
-import userRoutes from './routes.jnpm install --save sequelizes'
+import express from "express";
+import User from "./models/User.js";
+import Sequelize from "sequelize";
+import config from "./config/database.js";
+import userRoutes from './routes.js'
 
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+const sequelize = new Sequelize(config);
+User.init(sequelize);
 
-app.use('/usuarios', userRoutes)
+app.use("/usuarios", userRoutes);
 
-app.listen(1000, () => {
-    console.log('Server On-line')
-})
-
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conectado ao banco de dados");
+    app.listen(1000, () => {
+      console.log("Server On-line");
+    });
+  })
+  .catch((err) => {
+    console.err(err);
+  });
